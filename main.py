8f35745e69ea1f7e012de63ac81dfaa2d77918dd
Ninyo97 <31740051+Ninyo97@users.noreply.github.com>
2025-08-10 02:56:03 +0530

    3

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import torch
+import torch.nn as nn
 import torchvision
 from torch.utils.data import Dataset, DataLoader
 import os
@@ -24,3 +26,32 @@ print(isinstance(trainset, Dataset))
 
 traindl = DataLoader(dataset=trainset, batch_size=1, shuffle=True)
 testdl = DataLoader(dataset=testset, batch_size=1, shuffle=False)
+
+
+class MLP(torch.nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+
+        self.fc1 = nn.Linear(in_features=self.in_features, out_features=50)
+        self.activation = nn.ReLU()
+        self.fc2 = nn.Linear(in_features=50, out_features=out_features)
+
+    def forward(self, x):
+        '''
+        x.shape = (n, 1, 28, 28)
+        out.shape = (n, 1)
+        '''
+        assert ((x.shape[2] == 28) and (x.shape[3] == 28), f"Input shape (n, 1, {x.shape[2]}, {x.shape[3]}) does not match expected shape (n, 1, 28, 28)")
+
+        x = torch.flatten(x, start_dim=1, end_dim=-1)
+        
+        out = self.fc1(x)
+        out = self.activation(out)
+        out = self.fc2(out)
+        return out
+
+
+
+model = MLP(in_features=784, out_features=10)
--- readme.md
+++ readme.md
@@ -9,3 +9,10 @@ Dataset: A collection of samples $\{(x_i, y_i)\}$.
 ### Step 2: torch.utils.data.Dataloader
 
 The Dataloader handles batching and dataset shuffling. A dataset is wrapped with a dataloader object.
+
+### Step 3: Pytorch setup
+
+- Tensors and `dir(torch.Tensor)`.
+- The model is always implemented as a class that inherits `torch.nn.Module` object. This way, the model inherits a lot of internal methods and objects useful for ML (weights, backward and forward calls!).
+- We generally need to implement the __init__ and forward functions to implement a model.
+- Explore the model. Checkout `dir(model)`.
